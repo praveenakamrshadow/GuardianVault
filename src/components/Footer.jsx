72026e568f6d19a30a88b1848c7b7dc4bf3ea1bf
praveen Sahu <praveen.pkyt327867@gmail.com>
2024-03-22 18:34:43 +0530

    final commit with major code changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <div className="bg-slate-800 text-white flex flex-col justify-center items-center fixed bottom-0 w-full">
+        <div className="bg-slate-800 text-white flex flex-col justify-center items-center  bottom-0 w-full">
             <div className="logo font-bold text-2xl">
                 <span className="text-green-700">&lt;</span>
                 Guardian
--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,8 +1,6 @@
-import { useEffect } from 'react';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import { v4 as uuidv4 } from 'uuid';
-
 import 'react-toastify/dist/ReactToastify.css';
 
 const Manager = () => {
@@ -12,7 +10,7 @@ const Manager = () => {
     const ref = useRef();
 
     useEffect(() => {
-        let passwords = localStorage.getItem('passwords');
+        const passwords = localStorage.getItem('passwords');
         if (passwords) {
             setPasswordArray(JSON.parse(passwords));
         }
@@ -27,27 +25,35 @@ const Manager = () => {
     };
 
     const savePassword = () => {
+        if (!form.site || !form.username || !form.password) {
+            showErrorToast('Please fill in all fields.');
+            return;
+        }
+
         const newPassword = { ...form, id: uuidv4() };
-        setPasswordArray((prevPasswords) => {
-            if (Array.isArray(prevPasswords)) {
-                return [...prevPasswords, newPassword];
-            } else {
-                return [newPassword];
-            }
-        });
+        setPasswordArray((prevPasswords) => [...prevPasswords, newPassword]);
         setForm({ site: '', username: '', password: '' });
+        showSuccessToast('Password saved successfully!');
     };
 
     const deletePassword = (id) => {
-        console.log('deleting password with id', id);
+        const confirmDelete = window.confirm(
+            'Are you sure you want to delete this password?'
+        );
+        if (confirmDelete) {
+            setPasswordArray((prevPasswords) =>
+                prevPasswords.filter((p) => p.id !== id)
+            );
+            showSuccessToast('Password Deleted!');
+        }
+    };
+
+    const editPassword = (id) => {
+        const passwordToEdit = passwordArray.find((p) => p.id === id);
+        setForm(passwordToEdit);
         setPasswordArray((prevPasswords) =>
             prevPasswords.filter((p) => p.id !== id)
         );
-    };
-    const editPassword = (id) => {
-        console.log('editing password with id', id);
-        setForm(passwordArray.filter((p) => p.id === id)[0]);
-        setPasswordArray(passwordArray.filter((p) => p.id !== id));
     };
 
     const handleChange = (e) => {
@@ -55,40 +61,33 @@ const Manager = () => {
     };
 
     const copyText = (text) => {
-        toast.success('Copied to clipboard!', {
-            position: 'top-right',
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: 'colored',
-        });
         navigator.clipboard.writeText(text);
+        showSuccessToast('Copied to clipboard!');
     };
+
+    const showErrorToast = (message) => {
+        toast.error(message, getToastConfig());
+    };
+
+    const showSuccessToast = (message) => {
+        toast.success(message, getToastConfig());
+    };
+
+    const getToastConfig = () => ({
+        position: 'top-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'dark',
+    });
 
     return (
         <>
-            <ToastContainer
-                position="top-right"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="light"
-                transition="Bounce"
-            />
-            {/* Same as */}
-            <ToastContainer />
-            <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
-                <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-green-400 opacity-20 blur-[100px]"></div>
-            </div>
-            <div className="  mycontainer">
+            <ToastContainer {...getToastConfig()} />
+            <div className="p-3 md:p-0 md:mycontainer min-h-[84vh] md:min-h-[84vh]">
                 <h1 className="text-4xl font-bold text-center ">
                     <span className="text-green-500">&lt;</span>
                     Guardian
@@ -97,12 +96,11 @@ const Manager = () => {
                 <p className="text-green-900 text-lg text-center">
                     Safeguarding Your Digital Keys
                 </p>
-                <div className=" flex flex-col p-4 text-black gap-8 items-center">
+                <div className="flex flex-col p-4 text-black gap-8 items-center">
                     <input
                         className="rounded-full border border-green-500 w-full px-4 py-1"
                         type="text"
                         name="site"
-                        id="site"
                         value={form.site}
                         onChange={handleChange}
                         placeholder="Enter Website URL"
@@ -112,7 +110,6 @@ const Manager = () => {
                             type="text"
                             className="rounded-full border border-green-500 w-full px-4 py-1"
                             name="username"
-                            id="username"
                             value={form.username}
                             onChange={handleChange}
                             placeholder="Enter Username"
@@ -122,7 +119,6 @@ const Manager = () => {
                                 type={showPassword ? 'text' : 'password'}
                                 className="rounded-full border border-green-500 w-full px-4 py-1"
                                 name="password"
-                                id="password"
                                 value={form.password}
                                 onChange={handleChange}
                                 placeholder="Enter Password"
@@ -145,7 +141,6 @@ const Manager = () => {
                             </span>
                         </div>
                     </div>
-
                     <button
                         onClick={savePassword}
                         className="gap-2 flex justify-center items-center bg-green-400 hover:bg-green-300 rounded-full w-fit px-6 py-2 border border-green-900"
@@ -158,12 +153,14 @@ const Manager = () => {
                     </button>
                 </div>
                 <div className="passwords">
-                    <h2 className="font-bold text-2xl py-4">Your Passwords</h2>
+                    <h2 className="font-bold text-2xl py-4 flex justify-center">
+                        Your Passwords
+                    </h2>
                     {passwordArray.length === 0 && (
                         <div>No Passwords to show</div>
                     )}
-                    {passwordArray.length != 0 && (
-                        <table className="table-auto w-full rounded-md overflow-hidden">
+                    {passwordArray.length !== 0 && (
+                        <table className="table-auto w-[99%] mx-auto rounded-md overflow-hidden mb-10">
                             <thead className="bg-green-800 text-white">
                                 <tr>
                                     <th className="py-2">Site</th>
@@ -186,9 +183,9 @@ const Manager = () => {
                                                 </a>
                                                 <div
                                                     className="lordiconcopy size-7 cursor-pointer"
-                                                    onClick={() => {
-                                                        copyText(item.site);
-                                                    }}
+                                                    onClick={() =>
+                                                        copyText(item.site)
+                                                    }
                                                 >
                                                     <lord-icon
                                                         style={{
@@ -208,9 +205,9 @@ const Manager = () => {
                                                 <span>{item.username}</span>
                                                 <div
                                                     className="lordiconcopy size-7 cursor-pointer"
-                                                    onClick={() => {
-                                                        copyText(item.username);
-                                                    }}
+                                                    onClick={() =>
+                                                        copyText(item.username)
+                                                    }
                                                 >
                                                     <lord-icon
                                                         style={{
@@ -230,9 +227,9 @@ const Manager = () => {
                                                 <span>{item.password}</span>
                                                 <div
                                                     className="lordiconcopy size-7 cursor-pointer"
-                                                    onClick={() => {
-                                                        copyText(item.password);
-                                                    }}
+                                                    onClick={() =>
+                                                        copyText(item.password)
+                                                    }
                                                 >
                                                     <lord-icon
                                                         style={{
@@ -250,9 +247,9 @@ const Manager = () => {
                                         <td className=" text-center border border-white py-2">
                                             <span
                                                 className="cursor-pointer mx-1"
-                                                onClick={() => {
-                                                    editPassword(item.id);
-                                                }}
+                                                onClick={() =>
+                                                    editPassword(item.id)
+                                                }
                                             >
                                                 <lord-icon
                                                     src="https://cdn.lordicon.com/wuvorxbv.json"
@@ -265,9 +262,9 @@ const Manager = () => {
                                             </span>
                                             <span
                                                 className="cursor-pointer mx-1"
-                                                onClick={() => {
-                                                    deletePassword(item.id);
-                                                }}
+                                                onClick={() =>
+                                                    deletePassword(item.id)
+                                                }
                                             >
                                                 <lord-icon
                                                     src="https://cdn.lordicon.com/drxwpfop.json"
