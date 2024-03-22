cc61366984dcf916f0162574ea323a05461e28fe
praveen Sahu <praveen.pkyt327867@gmail.com>
2024-03-22 11:49:40 +0530

    navbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import Manager from './components/Manager';
+import Navbar from './components/Navbar';
 
 const App = () => {
-    return <div>App</div>;
+    return (
+        <div>
+            <Navbar />
+            <Manager />
+        </div>
+    );
 };
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+const Navbar = () => {
+    return (
+        <nav className="bg-slate-800 text-white">
+            <div className="mycontainer flex justify-between items-center px-4 py-5 h-14">
+                <div className="logo font-bold text-2xl">
+                    <span className="text-green-700">&lt;</span>
+                    Password
+                    <span className="text-green-700">Manager /&gt;</span>
+                </div>
+                <ul>
+                    <li className="flex gap-4">
+                        <a className="hover:font-bold" href="#">
+                            Home
+                        </a>
+                        <a className="hover:font-bold" href="#">
+                            About
+                        </a>
+                        <a className="hover:font-bold" href="#">
+                            Home
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
